7d5bd2d7441fe21e04e5a79379ca9063a6861953
vamit7631 <37224005+vamit7631@users.noreply.github.com>
2019-02-13 10:55:07 +0530

    Update registercontroller.js

--- controller/registercontroller.js
+++ controller/registercontroller.js
@@ -1,43 +1,32 @@
-var Agenda = require('agenda');
-var agenda = new Agenda();
-var registerserviceObj = require('../model/register');
+const registerserviceObj = require('../model/register');
+const registervalidateObj = require('../validation/registervalidate')
 
-
-module.exports.testfunction = function (req, res) {
-    var testdata = new registerserviceObj({
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        useremail : req.body.useremail,
-        dob: req.body.dob,
-        gender: req.body.gender
-      });
-      testdata.save(function(err) {
-        if (err) {
-            res.send(err)
-        }
-        else{
-            res.send('User saved successfully!');
-        }
-      });
+module.exports.testfunction = async function (req, res) {  
+    try{
+        await registervalidateObj.createValidation(req);  
+        let testdata = new registerserviceObj({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            useremail : req.body.useremail,
+            dob: req.body.dob,
+            gender: req.body.gender
+          });
+          
+          testdata.save(function(err) {
+            if (err) {
+                res.send(err)
+            }
+            else{
+                res.send('User saved successfully!');
+            }
+          });
+    }catch(e){
+        res.send(e);
+    }
 }
 
-
-// async function run() {
-  
-//     const agenda = new Agenda();
-
-//     agenda.define('hello', () => {
-//       console.log('Hello, World!');
-//       process.exit(0);
-//     });
-  
-
-//     await new Promise(resolve => agenda.once('ready', resolve));
-//     agenda.schedule('in 30 seconds', 'hello');
-//     agenda.start();
-//   }
-  
-//   run().catch(error => {
-//     console.error(error);
-//     process.exit(-1);
-//   });
+module.exports.findregisterData = function(req,res){
+    registerserviceObj.findregisterData(req, function(req,result){
+        
+    })
+}
