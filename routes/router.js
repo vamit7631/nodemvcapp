e18dba4d67d2d3c17d52bb18a38eb36c6d9230a4
vamit7631 <37224005+vamit7631@users.noreply.github.com>
2019-02-13 17:40:16 +0530

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -3,10 +3,64 @@ const router = express.Router();
 
 const userregistration = require('../controller/registercontroller')
 
+/**
+ * @swagger
+ * definitions:
+ *   TestSite:
+ *     properties:
+ *      firstname:
+ *          type: string
+ */
+
+
+
+
+/**
+ * @swagger
+ * /registration/register:
+ *   post:
+ *     tags:
+ *       - Register
+ *     description: Creates Registration
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: Register
+ *         description: Register object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           type: object
+ *           $ref: '#/definitions/Register'
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ */
+
 
 router.post('/registration/register/', userregistration.testfunction);
+
+/**
+ * @swagger
+ * /registration/getdetails:
+ *   get:
+ *     tags:
+ *       - Register
+ *     description: GetDetails Registration
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: firstname
+ *         description: firstname details
+ *         in: query
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: List of Registration Details
+ * 
+ */
 
 router.get('/registration/getdetails/',userregistration.findregisterData);
 
 module.exports = router;
-
