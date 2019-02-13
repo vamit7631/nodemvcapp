cc3e09dbcc81915d38cc5dec253891992e091778
vamit7631 <37224005+vamit7631@users.noreply.github.com>
2019-02-13 17:42:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 const routes = require('./routes/router')
-
+const swaggerUi = require('swagger-ui-express')
+const swaggerJSDoc = require('swagger-jsdoc');
 
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine','html');
@@ -14,6 +15,33 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+
+
+const swaggerDefinition = {
+  info: {
+    title: 'Test Development', 
+    version: '1.0.0',
+    description: 'A sample API', 
+  },
+  host: "localhost:3000",
+  basePath: '/', 
+};
+
+const options = {
+  swaggerDefinition: swaggerDefinition,
+  explorer: true,
+  apis: ['./routes/*.js'], 
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+app.get('/swagger.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/', routes)
 
