96f5dc25437cb12cf916f9c93446d6c5c8e7dd68
vamit7631 <37224005+vamit7631@users.noreply.github.com>
2019-02-19 18:36:35 +0530

    Update registerservice.js

--- services/registerservice.js
+++ services/registerservice.js
@@ -1,5 +1,11 @@
 const registermodelObj = require('../model/register');
 
+
+module.exports.testfunction = async function (data){ 
+    return await registermodelObj.create(data)
+ }
+ 
+
 module.exports.findregisterData = function(req,cb){
     let condition = { firstname : req.query.firstname};
     registermodelObj.find(condition, function(err, result){
