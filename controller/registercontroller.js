e9527efac5435b8d394e491fdaae56a33ffab8ce
vamit7631 <37224005+vamit7631@users.noreply.github.com>
2019-02-19 18:35:47 +0530

    Update registercontroller.js

--- controller/registercontroller.js
+++ controller/registercontroller.js
@@ -1,36 +1,22 @@
 const registerserviceObj = require('../services/registerservice');
 const registervalidateObj = require('../validation/registervalidate')
 
-module.exports.testfunction = async function (req, res) {  
-    try{
-        await registervalidateObj.createValidation(req);  
-        let testdata = new registerserviceObj({
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            useremail : req.body.useremail,
-            dob: req.body.dob,
-            gender: req.body.gender
-          });
-          
-          testdata.save(function(err) {
-            if (err) {
-                res.send(err)
-            }
-            else{
-                res.send('User saved successfully!');
-            }
-          });
-    }catch(e){
-        res.send(e);
+module.exports.testfunction = async function (req, res) {
+    try {
+        await registervalidateObj.createValidation(req.body);
+        let result = await registerserviceObj.testfunction(req.body);
+        return res.status(200).json({ "status": "Sucess", "data": result });
+    } catch (e) {
+        return res.status(400).json({ 'err': e });
     }
 }
 
-module.exports.findregisterData = function(req,res){
+module.exports.findregisterData = function (req, res) {
     registerserviceObj.findregisterData(req, function (err, result) {
         if (err) {
-            return  res.status(400).json({ 'err': err });
+            return res.status(400).json({ 'err': err });
         } else {
-            return  res.status(200).json(result);
+            return res.status(200).json(result);
         }
     })
 }
