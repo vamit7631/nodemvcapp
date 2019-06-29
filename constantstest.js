d76ebe4e2d3da7934a60a81f8f36887ed2287f1c
vamit7631 <37224005+vamit7631@users.noreply.github.com>
2019-06-29 10:12:58 +0000

    test constant

---
+++ constantstest.js
@@ -0,0 +1,26 @@
+//  HTTP Codes
+module.exports.HTTP_200 = 200
+module.exports.SUCCESS = 'success'
+module.exports.ERROR = 'error'
+module.exports.HTTP_401 = 401
+module.exports.INVALID_SESSION = 'Unauthorized, login to access this resource.'
+module.exports.HTTP_403 = 403
+module.exports.INVALID_USER = 'Forbidden, you do not have the required permission for this resource.'
+module.exports.HTTP_404 = 404
+module.exports.NOT_FOUND = 'Not found.'
+//  Global Constants
+module.exports.WELCOME_MSG = 'स्वागत हे'
+module.exports.VERSION_1 = 'v1'
+module.exports.SEPERATOR = '/'
+module.exports.ID = ':id'
+module.exports.VALID_TOKEN = 'Token is valid.'
+module.exports.INVALID_TOKEN = 'Token is not valid.'
+module.exports.MISSING_TOKEN = 'Token is not found.'
+//  Resource Sepcific Constants
+//  user
+module.exports.USER_RESOURCE = 'user'
+module.exports.USER_CREATED = 'User created.'
+module.exports.USER_NOT_FOUND = 'User not found.'
+//  auth
+module.exports.AUTH_RESOURCE = 'auth'
+module.exports.APP_ID = '5c04e06e657f930011a93ad9'
